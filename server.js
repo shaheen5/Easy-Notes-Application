114a17cd20e6a53d806816604191360fbd2c1228
shaheen5 <miyashaheen1995@gmail.com>
2021-06-11 22:45:24 +0530

    feat : added code to configure & connect to database

--- server.js
+++ server.js
@@ -9,6 +9,23 @@ app.use(express.urlencoded({extended: true}))
 //parse requests of content-type application/json
 app.use(express.json())
 
+//configuring the database
+const dbConfig = require('./config/database.config.js');
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+//connecting to the database
+mongoose.connect(dbConfig.url,{
+    useNewUrlParser:true,
+    useUnifiedTopology: true
+}).then(()=>{
+    console.log("Successfully connected to the database");
+}).catch(err=>{
+    console.log('Could not connect to the database. Exiting now...',err);
+    process.exit();
+});
+
 //define a simple route
 app.get('/',(req,res)=>{
     res.json({"message":"Welcome to EasyNotes application.Take notes quickly. Organize and keep track of all your notes."});
